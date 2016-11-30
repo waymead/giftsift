4eec52d47017dcc2cb25e9a8bce5dda39f19df2f
digbyk <digby@digby.net>
2016-11-30 22:06:16 +0000

    Improving admin pages

--- routes/admin.js
+++ routes/admin.js
@@ -10,26 +10,36 @@ var router = express.Router();
 
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
 	if (req.user && req.user.roles && req.user.roles.indexOf('admin') >= 0) {
 		next();
 	} else {
 		res.sendStatus(403);
 	}
-	
+
 });
 
 router.get('/', ensureLoggedIn, function (req, res) {
 	var allLists;
-	List.find({ }, {}, { sort: 'name' })
-		.then(function(lists) {
+	List.find({}, {}, { sort: 'name' })
+		.then(function (lists) {
 			allLists = lists;
-			return Gift.find({ }, {}, { sort: 'name' });
+			return Gift.find({}, {}, { sort: 'name' });
 		})
-		.then(function(gifts) {
+		.then(function (gifts) {
 			res.render('admin/index', { lists: allLists, gifts: gifts });
 		})
-		.catch(function(err) {
+		.catch(function (err) {
+			logger.error(err);
+		});
+});
+
+router.get('/lists', ensureLoggedIn, function (req, res) {
+	List.find({}, {}, { sort: 'name' })
+		.then(function (lists) {
+			res.render('admin/lists', { lists: lists });
+		})
+		.catch(function (err) {
 			logger.error(err);
 		});
 });
