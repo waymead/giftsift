492e970c7e2bb4ad4e7daca59f74fe18dea6cc78
digbyk <digby@digby.net>
2016-12-08 21:43:15 +0000

    Trying something

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,33 @@
 'use strict';
 
+var env = {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+};
+
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-	res.render('index', { error: req.flash('error') });
+router.get('/', function(req, res) {
+    res.render('index', { error: req.flash('error') });
 });
 
-router.get('/login', function (req, res) {
-	res.render('login', {
-		error: req.flash('error'),
-		returnTo: req.flash('ret', req.session.returnTo),
-		redirectTo: req.session.redirectTo
-	});
+router.get('/login', function(req, res) {
+    if (req.user) {
+        res.locals.user = req.user;
+    }
+    res.locals.env = env;
+    res.render('login', {
+        error: req.flash('error'),
+        returnTo: req.flash('ret', req.session.returnTo),
+        redirectTo: req.session.redirectTo
+    });
 });
 
-router.get('/logout', function (req, res) {
-	req.logout();
-	res.redirect('/');
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
