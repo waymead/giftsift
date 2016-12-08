74d4185ba6e49687d648db96b9491ac5e5c4379f
digbyk <digby@digby.net>
2016-12-08 21:50:04 +0000

    REverting

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var env = {
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-};
-
 var express = require('express');
 var router = express.Router();
 
@@ -14,11 +8,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/login', function(req, res) {
-    if (req.user) {
-        res.locals.user = req.user;
-    }
-    res.locals.env = env;
-    res.render('login', {
+     res.render('login', {
         error: req.flash('error'),
         returnTo: req.flash('ret', req.session.returnTo),
         redirectTo: req.session.redirectTo
