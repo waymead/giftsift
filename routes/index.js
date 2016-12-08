f8e0cf62c14d2f37919515e0860841b541bfc636
digbyk <digby@digby.net>
2016-12-08 21:57:38 +0000

    CHanges

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.render('index', { error: req.flash('error') });
+router.get('/', function (req, res) {
+	res.render('index', { error: req.flash('error') });
 });
 
-router.get('/login', function(req, res) {
-     res.render('login', {
-        error: req.flash('error'),
-        returnTo: req.flash('ret', req.session.returnTo),
-        redirectTo: req.session.redirectTo
-    });
+router.get('/login', function (req, res) {
+	res.render('login', {
+		error: req.flash('error'),
+		returnTo: req.flash('ret', req.session.returnTo),
+		redirectTo: req.session.redirectTo
+	});
 });
 
-router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+router.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
