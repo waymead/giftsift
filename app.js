9852ec8c42abefb8b2c5eaad44cbffc8bc2a6a59
digbyk <digby@digby.net>
2017-02-23 06:47:05 +0000

    CSP and referrer fixes

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ app.use(bodyParser.json());
 app.use(favicon(__dirname + '/public/images/logo.png'));
 app.use(helmet());
 app.use(helmet.referrerPolicy({ 
-	policy: 'same-origin'
+	policy: 'origin'
 }));
 app.use(helmet.contentSecurityPolicy({
 	directives: {
 		defaultSrc: ['\'self\''],
 		styleSrc: ['\'self\'', '\'unsafe-inline\'', 'cdnjs.cloudflare.com', 'code.getmdl.io', 'fonts.googleapis.com'],
-		scriptSrc: ['\'self\'', '\'unsafe-inline\'', '\'unsafe-eval\'', 'ajax.cloudflare.com', 'cdnjs.cloudflare.com', 'code.getmdl.io', 'js-agent.newrelic.com', 'ssl.google-analytics.com', 'bam.nr-data.net'],
+		scriptSrc: ['\'self\'', '\'unsafe-inline\'', '\'unsafe-eval\'', 'cdn.auth0.com.com', 'ajax.cloudflare.com', 'cdnjs.cloudflare.com', 'code.getmdl.io', 'js-agent.newrelic.com', 'ssl.google-analytics.com', 'bam.nr-data.net'],
 		fontSrc: ['\'self\'', 'fonts.gstatic.com'],
 		connectSrc: ['api.rollbar.com'],
 		imgSrc: ['*']
