f467be8761f13349ec0cf188c27659765344bc71
digbyk <digby@digby.net>
2017-10-07 13:08:54 +0100

    Fixed that ancient mongo warning

--- model/index.js
+++ model/index.js
@@ -9,33 +9,33 @@ const List = require('./list.js');
 const Gift = require('./gift.js');
 
 mongoose.connect(url, {
-	server: {
-		auto_reconnect: true
-	}
+	autoReconnect: true,
+	useMongoClient: true,
+	promiseLibrary: global.Promise
 });
 
-mongoose.connection.on('connected', function () {
-	logger.log('info', 'Mongoose connected');
-});
+// mongoose.connection.on('connected', function() {
+// 	logger.log('info', 'Mongoose connected');
+// });
 
-mongoose.connection.on('error', function (err) {
-	logger.info('Mongoose connection error: ' + err);
-	mongoose.connect(url, {
-		server: {
-			auto_reconnect: true
-		}
-	});
-});
+// mongoose.connection.on('error', function(err) {
+// 	logger.info('Mongoose connection error: ' + err);
+// 	mongoose.connect(url, {
+// 		server: {
+// 			auto_reconnect: true
+// 		}
+// 	});
+// });
 
-mongoose.connection.on('disconnected', function () {
-	logger.info('Mongoose disconnected');
-});
+// mongoose.connection.on('disconnected', function() {
+// 	logger.info('Mongoose disconnected');
+// });
 
-process.on('SIGINT', function() {
-	mongoose.connection.close(function() {
-		logger.info('Mongoose disconnected through app termination');
-		process.exit(0);
-	});
-});
+// process.on('SIGINT', function() {
+// 	mongoose.connection.close(function() {
+// 		logger.info('Mongoose disconnected through app termination');
+// 		process.exit(0);
+// 	});
+// });
 
 module.exports = { User: User, List: List, Gift: Gift, mongoose: mongoose };
