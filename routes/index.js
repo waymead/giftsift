4478c706d8e92f04dc037410e6d7470c1be7fda0
digbyk <digby@digby.net>
2017-10-07 14:14:00 +0100

    Home page changes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-//const logger = require('../lib/logging.js');
+const logger = require('../lib/logging');
 const Router = require('koa-router');
 const router = new Router();
 const authRouter = require('./auth');
@@ -6,11 +6,16 @@ const listsRouter = require('./lists');
 const giftsRouter = require('./gifts');
 const adminRouter = require('./admin');
 
-router.get('/', async ctx => {
-	if (ctx.isAuthenticated()) {
-		return ctx.redirect('/lists');
-	} else {
-		return ctx.render('index', { message: 'root' });
+router.get('/', async (ctx, next) => {
+	try {
+		if (ctx.isAuthenticated()) {
+			return ctx.redirect('/lists');
+		} else {
+			return ctx.render('index', { });
+		}
+	} catch (error) {
+		logger.error(error);
+		return next(error);
 	}
 });
 
